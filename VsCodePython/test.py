28ff4f2160b4a5a3e1af35c851af2d466c338952
Jan Schaffranek <jan.schaffranek@rub.de>
2020-05-05 19:41:34 +0200

    add

--- VsCodePython/test.py
+++ VsCodePython/test.py
@@ -4,3 +4,12 @@ print("Hello World!")
 
 my_number = random.randint(1, 10)
 print(my_number)
+
+import unittest
+
+class MyTest(unittest.TestCase):
+    def test_print(self):
+        assert(1 == True)
+
+    def test_print2(self):
+        assert(1 == True)
